45bbf85c63a7f963ed0e775dd20d5d742d467497
root <root@localhost.localdomain>
2018-03-14 04:12:58 +0800

    20180314 up

--- cmdb1_demo1/cmdb1_demo1/urls.py
+++ cmdb1_demo1/cmdb1_demo1/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import cmdb1_demo1_app1.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',cmdb1_demo1_app1.views.HelloWorld),
+    
 ]
--- cmdb1_demo1/cmdb1_demo1_app1/views.py
+++ cmdb1_demo1/cmdb1_demo1_app1/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+#coding:utf8
+from django.shortcuts import render,HttpResponseRedirect,render_to_response
+from django.http import HttpResponse
+import time
 
 # Create your views here.
+def HelloWorld ( request ) :
+    return HttpResponse ( 'hello world!' )
+
+
+
+
+
